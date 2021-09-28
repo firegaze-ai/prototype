2156e9f6a4f728a333c9db6ad31bd67a66dede35
Orfeas Kypris <kypriso@gmail.com>
2021-09-28 11:48:35 +0300

    refactor

--- yolo/convert_dataset_to_yolo.py
+++ yolo/convert_dataset_to_yolo.py
@@ -1,6 +1,6 @@
 import argparse
 import shutil
-from typing import Dict, Optional
+from typing import Dict, Optional, List, Tuple
 
 import numpy as np
 import os
@@ -8,12 +8,48 @@ import glob
 import simplejson as json
 
 
+def calculate_split_ratios(split_percent: Dict[str, float], all_data: List[str], image_or_scene: str) -> \
+        Tuple[int, int, int]:
+    no_training_examples = int(split_percent["train"] * len(all_data))
+    print("Number of training {}: {}".format(image_or_scene, no_training_examples))
+    no_val_examples = int(split_percent["val"] * len(all_data))
+    print("Number of validation {}: {}".format(image_or_scene, no_val_examples))
+    no_test_examples = len(all_data) - (no_training_examples + no_val_examples)
+    print("Number of test {}: {}".format(image_or_scene, no_test_examples))
+    return no_training_examples, no_val_examples, no_test_examples
+
+
+def split_into_subsets(all_data: List[str], no_training_examples: int, no_val_examples: int,
+                       no_test_examples: int) -> \
+        Tuple[int, int, int]:
+    train_scenes = np.random.choice(all_data, size=no_training_examples, replace=False)
+    remaining_scenes = [scene for scene in all_data if scene not in train_scenes]
+    val_scenes = np.random.choice(remaining_scenes, size=no_val_examples, replace=False)
+    test_scenes = [scene for scene in remaining_scenes if scene not in val_scenes]
+    return train_scenes, val_scenes, test_scenes
+
+
+def copy_images_and_labels(key: str, path_to_image: str, path_to_dataset_dir: str, path_to_output_dir: str) -> \
+Tuple[str, str]:
+    image_filename = os.path.split(path_to_image)[-1]
+    shutil.copy(path_to_image, os.path.join(path_to_output_dir, key, "images", image_filename))
+
+    # copy label to labels folder
+    label_filename = os.path.splitext(os.path.basename(path_to_image))[0] + ".txt"
+    path_to_label = os.path.join(path_to_dataset_dir, "labels", label_filename)
+    shutil.copy(path_to_label,
+        os.path.join(path_to_output_dir, key, "labels", os.path.split(path_to_label)[-1]))
+    return image_filename, label_filename
+
+
 def main(path_to_dataset_dir: str, path_to_output_dir: str, input_version: str,
-         output_version: str,
+         output_version: str, split_by_scene: bool,
          dataset_json: Optional[str],
          split_percent: Dict,
          class_name: Optional[str] = "Smoke"):
     # TODO: enable dataset replication functionality
+
+    print(split_by_scene)
 
     # collect scenes from dataset dir
     all_data_sources = glob.glob(os.path.join(path_to_dataset_dir, "images", "*"))
@@ -21,18 +57,30 @@ def main(path_to_dataset_dir: str, path_to_output_dir: str, input_version: str,
     for source_folder in all_data_sources:
         all_scenes += glob.glob(os.path.join(source_folder, "*"))
 
-    # split scenes into train, val, test
-    no_training_examples = int(split_percent["train"] * len(all_scenes))
-    print("Number of training scenes: {}".format(no_training_examples))
-    no_val_examples = int(split_percent["val"] * len(all_scenes))
-    print("Number of validation scenes: {}".format(no_val_examples))
+    if split_by_scene:
+        # split scenes into train, val, test
+        no_training_examples, no_val_examples, no_test_examples = calculate_split_ratios(split_percent,
+            all_scenes, image_or_scene="scenes")
 
-    no_test_examples = len(all_scenes) - (no_training_examples + no_val_examples)
-    print("Number of test scenes: {}".format(no_test_examples))
-    train_scenes = np.random.choice(all_scenes, size=no_training_examples, replace=False)
-    remaining_scenes = [scene for scene in all_scenes if scene not in train_scenes]
-    val_scenes = np.random.choice(remaining_scenes, size=no_val_examples, replace=False)
-    test_scenes = [scene for scene in remaining_scenes if scene not in val_scenes]
+        train_data, val_data, test_data = split_into_subsets(all_scenes, no_training_examples,
+            no_val_examples, no_test_examples)
+
+        print(train_data, val_data, test_data)
+
+    else:
+        all_images = []
+        for datapoint in all_scenes:
+            all_images += glob.glob(os.path.join(datapoint, "*"))
+        print(all_images)
+
+        # split images into train, val, test
+        no_training_examples, no_val_examples, no_test_examples = calculate_split_ratios(split_percent,
+            all_images, image_or_scene="images")
+
+        train_data, val_data, test_data = split_into_subsets(all_images, no_training_examples,
+            no_val_examples, no_test_examples)
+
+        print(train_data, val_data, test_data)
 
     # make directories for train, val, test
     os.makedirs(os.path.join(path_to_output_dir, "train", "images"), exist_ok=True)
@@ -46,26 +94,29 @@ def main(path_to_dataset_dir: str, path_to_output_dir: str, input_version: str,
     dataset_log = {}
 
     # find corresponding label file for each image file
-    for id, split in {"train": train_scenes, "val": val_scenes, "test": test_scenes}.items():
+    for key, split in {"train": train_data, "val": val_data, "test": test_data}.items():
         all_images = []
         all_labels = []
-        for scene in split:
-            images = glob.glob(os.path.join(scene, "*"))
-            for image in images:
-                # copy image to images folder
-                image_filename = os.path.split(image)[-1]
-                shutil.copy(image, os.path.join(path_to_output_dir, id, "images", image_filename))
-
-                # copy label to labels folder
-                label_filename = os.path.splitext(os.path.basename(image))[0] + ".txt"
-                path_to_label = os.path.join(path_to_dataset_dir, "labels", label_filename)
-                shutil.copy(path_to_label,
-                    os.path.join(path_to_output_dir, id, "labels", os.path.split(path_to_label)[-1]))
+        for datapoint in split:
+            if split_by_scene:
+                images = glob.glob(os.path.join(datapoint, "*"))
+                for image in images:
+                    image_filename, label_filename = copy_images_and_labels(key, image, path_to_dataset_dir,
+                        path_to_output_dir)
+                    all_images.append(image_filename)
+                    all_labels.append(label_filename)
+            else:
+                image = datapoint
+                image_filename, label_filename = copy_images_and_labels(key, image, path_to_dataset_dir,
+                    path_to_output_dir)
                 all_images.append(image_filename)
                 all_labels.append(label_filename)
 
-        dataset_log.update({id: {"images": all_images, "labels": all_labels}})
-    dataset_log.update({"dataset_version": input_version + "." + output_version})
+        dataset_log.update({key: {"images": all_images, "labels": all_labels}})
+    dataset_log.update({"is_split_by_scenes": True if split_by_scene else False})
+    dataset_log.update({"dataset_input_version": input_version })
+    dataset_log.update({"dataset_output_version": output_version})
+    dataset_log.update({"split_ratios": split_percent})
 
     print("Number of training images: {}\n"
           "Number of validation images: {}\n"
@@ -99,6 +150,11 @@ if __name__ == '__main__':
     parser.add_argument('-tr', '--train', help='Train split ratios', type=float, required=True)
     parser.add_argument('-v', '--val', help='Val split ratio', type=float, required=True)
     parser.add_argument('-te', '--test', help='Test split ratio', type=float, required=True)
+    # parser.add_argument('-s', '--split_by_scene',
+    #     help='Make sure train/val/test do not contain frames from the same scene', type=bool, required=True)
+    parser.add_argument('--split_by_scene', dest='split_by_scene', action='store_true')
+    parser.add_argument('--no-split_by_scene', dest='split_by_scene', action='store_false')
+    parser.set_defaults(feature=True)
     parser.add_argument('-iv', '--input_version', help='Input version of the dataset', required=True)
     parser.add_argument('-ov', '--output_version',
         help='Output version of the dataset (new combination of source data)', required=True)
@@ -115,4 +171,5 @@ if __name__ == '__main__':
         input_version=args["input_version"],
         output_version=args["output_version"],
         split_percent={"train": args["train"], "val": args["val"], "test": args["test"]},
+        split_by_scene=args["split_by_scene"],
         dataset_json=None)
